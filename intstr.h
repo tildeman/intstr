89dd4a20a47b99ec2ab5b2673c4f83446991c2b7
tildeman <59699056+tildeman@users.noreply.github.com>
2020-12-12 22:21:54 +0700

    Signed operations
    Added signed operations for negative numbers.

--- intstr.h
+++ intstr.h
@@ -1,18 +1,16 @@
 //Integer (unsigned) string operations for dummies
 #include <string>
 
+//Pre-built multiplication table
 int bcc[10][10]={{0,0,0,0,0,0,0,0,0,0},{0,1,2,3,4,5,6,7,8,9},{0,2,4,6,8,10,12,14,16,18},{0,3,6,9,12,15,18,21,24,27},{0,4,8,12,16,20,24,28,32,36},{0,5,10,15,20,25,30,35,40,45},{0,6,12,18,24,30,36,42,48,54},{0,7,14,21,28,35,42,49,56,63},{0,8,16,24,32,40,48,56,64,72},{0,9,18,27,36,45,54,63,72,81}};
 
-std::string strip_0s(std::string n){
-    if (n=="0") return n;
-    while (n[0]=='0') n.erase(0,1);
-    if (n=="") n="0";
-    return n;
-}
+// Utility functions
 
 std::string to_str(int n){
     std::string s;
     char c;
+    if (n<0) return "-"+to_str(-n);
+    if (n==0) return "0";
     while (n!=0){
         c=(n%10)+48;
         s=c+s;
@@ -21,7 +19,55 @@ std::string to_str(int n){
     return s;
 }
 
-int strcmp(std::string a,std::string b){
+std::string reduce_sign(std::string n){
+    int s=0,i;
+    for (i=0;i<n.length();i++){
+        if (n[i]=='+') s+=0;
+        else if (n[i]=='-') s=(s+1)%2;
+        else break;
+    }
+    while (n[0]=='+' or n[0]=='-') n.erase(0,1);
+    if (n=="0") return "0";
+    if (s==1) n='-'+n;
+    return n;
+}
+
+std::string strip_0s_u(std::string n){
+    n+='a';
+    while (n[0]=='0') n.erase(0,1);
+    if (n=="a") n="0";
+    else n.erase(n.length()-1,1);
+    return n;
+}
+
+int sign_check(std::string n){
+    n=reduce_sign(n);
+    if (n[0]=='-') return -1;
+    n=strip_0s_u(n);
+    if (n=="0") return 0;
+    return 1;
+}
+
+std::string strip_0s(std::string n){
+    n=reduce_sign(n);
+    if (sign_check(n)==-1){
+        n.erase(0,1);
+        n=strip_0s_u(n);
+        return '-'+n;
+    }
+    else if (sign_check(n)==0) return "0";
+    else if (sign_check(n)==1){
+        n=strip_0s_u(n);
+        return n;
+    }
+    return "0";
+}
+
+// Unsigned functions
+
+int strcmp_u(std::string a,std::string b){
+    a=strip_0s_u(a);
+    b=strip_0s_u(b);
     if (a.length()>b.length()) return -1;
     if (b.length()>a.length()) return  1;
     for (int i=0;i<a.length();i++){
@@ -31,7 +77,7 @@ int strcmp(std::string a,std::string b){
     return 0;
 }
 
-std::string stradd(std::string a, std::string b){
+std::string stradd_u(std::string a, std::string b){
     std::string c,t;
     char res;
     int i,j,carry=0;
@@ -48,7 +94,6 @@ std::string stradd(std::string a, std::string b){
         carry=j/10;
         res=(j%10)+48;
         c=res+c;
-        //cout << j;
     }
     if (carry){
         res=carry+48;
@@ -57,11 +102,11 @@ std::string stradd(std::string a, std::string b){
     return c;
 }
 
-std::string strsub(std::string a, std::string b){
+std::string strsub_u(std::string a, std::string b){
     std::string c,t;
     char res;
     int i,j,carry=0;
-    if (strcmp(a,b)==1) return "-"+strsub(b,a); //idk why I would do this but it's an upcoming feature.
+    if (strcmp_u(a,b)==1) return '-'+strsub_u(b,a);
     for (i=b.length();i<a.length();i++){
         b='0'+b;
     }
@@ -74,7 +119,6 @@ std::string strsub(std::string a, std::string b){
         }
         res=(j%10)+48;
         c=res+c;
-        //cout << j;
     }
     return strip_0s(c);
 }
@@ -84,53 +128,162 @@ std::string strmulb1d(std::string a, char b){
     int i;
     for (i=a.length()-1;i>=0;i--){
         d=to_str(bcc[a[i]-48][b-48])+zeros;
-        c=stradd(c,d);
+        c=stradd_u(c,d);
         zeros+='0';
     }
     return c;
 }
 
-std::string strmul(std::string a,std::string b){
+std::string strmul_u(std::string a,std::string b){
     std::string c="0",t,zeros;
     char res;
     int i,j,carry=0;
     for (i=a.length()-1;i>=0;i--){
         t=strmulb1d(b,a[i])+zeros;
-        c=stradd(c,t);
+        c=stradd_u(c,t);
         zeros+='0';
     }
     return strip_0s(c);
 }
 
 std::string strdivb1d(std::string a,std::string b){
-    if (strcmp(a,b)==1) return "0";
+    if (strcmp_u(a,b)==1) return "0";
     for (int i=1;i<10;i++){
-        if (strcmp(a,strmul(b,to_str(i)))==1) return to_str(i-1);
+        if (strcmp_u(a,strmul_u(b,to_str(i)))==1) return to_str(i-1);
     }
     return "9";
 }
 
-std::string strdiv(std::string a,std::string b){
-    if (strcmp(a,b)==1) return "0";
+std::string strdiv_u(std::string a,std::string b){
+    if (strcmp_u(a,b)==1) return "0";
     std::string r,d,rem;
     int i,n=a.length();
     for (i=0;i<a.length();i++){
         d=strdivb1d(strip_0s(rem+a[i]),b);
         r+=d;
-        rem=strsub(strip_0s(rem+a[i]),strmul(d,b));
-        //cout << rem << endl;
+        rem=strsub_u(strip_0s(rem+a[i]),strmul_u(d,b));
     }
-    return strip_0s(r);
+    return strip_0s_u(r);
+}
+
+std::string strmod_u(std::string a,std::string b){
+    std::string r,d,rem;
+    int i,n=a.length();
+    for (i=0;i<a.length();i++){
+        d=strdivb1d(strip_0s_u(rem+a[i]),b);
+        r+=d;
+        rem=strsub_u(strip_0s_u(rem+a[i]),strmul_u(d,b));
+    }
+    return strip_0s_u(rem);
+}
+
+// Signed functions
+
+std::string stradd(std::string a, std::string b){
+    a=strip_0s(reduce_sign(a));
+    b=strip_0s(reduce_sign(b));
+    int aa=sign_check(a), bb=sign_check(b);
+    if (aa>=0 and bb>=0) return stradd_u(a,b);
+    else if (aa<0 and bb>=0){
+        a.erase(0,1);
+        return strsub_u(b,a);
+    }
+    else if (aa>=0 and bb<0){
+        b.erase(0,1);
+        return strsub_u(a,b);
+    }
+    else if (aa<0 and bb<0){
+        a.erase(0,1);
+        b.erase(0,1);
+        return '-'+stradd_u(a,b);
+    }
+    return "0";
+}
+
+std::string strsub(std::string a, std::string b){
+    a=strip_0s(reduce_sign(a));
+    b=strip_0s(reduce_sign(b));
+    int aa=sign_check(a), bb=sign_check(b);
+    if (aa>=0 and bb>=0) return strsub_u(a,b);
+    else if (aa<0 and bb>=0){
+        a.erase(0,1);
+        return '-'+stradd_u(b,a);
+    }
+    else if (aa>=0 and bb<0){
+        b.erase(0,1);
+        return stradd_u(a,b);
+    }
+    else if (aa<0 and bb<0){
+        a.erase(0,1);
+        b.erase(0,1);
+        return strsub_u(b,a);
+    }
+    return "0";
+}
+
+std::string strmul(std::string a,std::string b){
+    a=strip_0s(reduce_sign(a));
+    b=strip_0s(reduce_sign(b));
+    int aa=sign_check(a),bb=sign_check(b);
+    if (aa>=0 and bb>=0) return strmul_u(a,b);
+    else if (aa>=0 and bb<0){
+        b.erase(0,1);
+        return '-'+strmul_u(a,b);
+    }
+    else if (aa<0 and bb>=0){
+        a.erase(0,1);
+        return '-'+strmul_u(a,b);
+    }
+    else if (aa<0 and bb<0){
+        a.erase(0,1);
+        b.erase(0,1);
+        return strmul_u(a,b);
+    }
+    return "0";
+}
+
+std::string strdiv(std::string a,std::string b){
+    a=strip_0s(reduce_sign(a));
+    b=strip_0s(reduce_sign(b));
+    int aa=sign_check(a),bb=sign_check(b);
+    if (aa>=0 and bb>=0) return strdiv_u(a,b);
+    else if (aa>=0 and bb<0){
+        b.erase(0,1);
+        return '-'+strdiv_u(a,b);
+    }
+    else if (aa<0 and bb>=0){
+        a.erase(0,1);
+        return '-'+strdiv_u(a,b);
+    }
+    else if (aa<0 and bb<0){
+        a.erase(0,1);
+        b.erase(0,1);
+        return strdiv_u(a,b);
+    }
+    return "0";
 }
 
 std::string strmod(std::string a,std::string b){
-    std::string r,d,rem;
-    int i,n=a.length();
-    for (i=0;i<a.length();i++){
-        d=strdivb1d(strip_0s(rem+a[i]),b);
-        r+=d;
-        rem=strsub(strip_0s(rem+a[i]),strmul(d,b));
-        //cout << rem << endl;
+    a=strip_0s(reduce_sign(a));
+    b=strip_0s(reduce_sign(b));
+    int aa=sign_check(a),bb=sign_check(b);
+    if (bb>=0){
+        if (aa>=0) return strmod_u(a,b);
+        else if (aa<0){
+            a.erase(0,1);
+            return strsub(b,strmod_u(a,b));
+        }
     }
-    return strip_0s(rem);
+    else if (bb<0){
+        if (aa>=0) {
+            b.erase(0,1);
+            return stradd(strsub("0",b),strmod_u(a,b));
+        }
+        else if (aa<0){
+            a.erase(0,1);
+            b.erase(0,1);
+            return strsub("0",strmod_u(a,b));
+        }
+    }
+    return "0";
 }
